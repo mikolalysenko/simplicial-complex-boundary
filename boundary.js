f4cba4961c4b10ee3c178e9f27ee1f13e04cb6ff
mikolalysenko <mikolalysenko@gmail.com>
2015-06-20 14:17:15 -0500

    refactor to use submodules, also preserve orientation

--- boundary.js
+++ boundary.js
@@ -2,17 +2,9 @@
 
 module.exports = boundary
 
-var sc = require('simplicial-complex')
+var bnd = require('boundary-cells')
+var reduce = require('reduce-simplicial-complex')
 
 function boundary(cells) {
-  var bnd = sc.boundary(cells)
-  var result = []
-  for(var i=0; i<bnd.length; ++i) {
-    if(i+1 < bnd.length && sc.compareCells(bnd[i], bnd[i+1]) === 0) {
-      ++i
-      continue
-    }
-    result.push(bnd[i])
-  }
-  return result
+  return reduce(bnd(cells))
 }
